ee8dfcc55d3b246418d3324f67c7e9ee5eb3785b
unknown <ashishbhandari380@gmail.com>
2025-09-13 11:03:46 +0545

    error fix vayo

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import './home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 const Home = () => {
   const [category,setCategory]=useState("All");
 
@@ -10,7 +9,7 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
-      {/* <FoodDisplay category={category}/> */}
+      
     </div>
   )
 }
